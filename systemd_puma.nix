76533cb145e74d9f14f0ac15de7ecc09dca2a44b
Your Name <sun@Evgenijs-MacBook-Pro.local>
2023-04-06 18:30:20 +0300

    use puma

--- systemd_puma.nix
+++ systemd_puma.nix
@@ -15,7 +15,7 @@
       Type = "notify";
       WatchdogSec = 10;
       WorkingDirectory = "/home/buhduh/app/current";
-      ExecStart = "/home/buhduh/bin/bundle exec puma -C /home/buhduh/app/current/config/puma.rb";
+      ExecStart = "/home/buhduh/bin/puma -C /home/buhduh/app/current/config/puma.rb";
       Restart = "always";
       User = "buhduh";
     };
