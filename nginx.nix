d31e6ca67e828a2caaaad712fd42574c986f4c3e
Your Name <sun@Evgenijs-MacBook-Pro.local>
2023-04-06 21:33:34 +0300

    update nginx for upstram

--- nginx.nix
+++ nginx.nix
@@ -1,13 +1,39 @@
 {
   security.acme.acceptTerms = true;
   security.acme.defaults.email = "varlamoved@gmail.com";
-  services.nginx.enable = true;
-  services.nginx.virtualHosts."buhduh.ru" = {
-    addSSL = true;
-    enableACME = true;
-    root = "/var/www/buhduh.ru";
+
+  services.nginx = {
+    enable = true;
+    upstreams.buhduh.servers."unix:///tmp/puma.sock" = {};
+    virtualHosts."buhduh.ru" = {
+      #   addSSL = true;
+      #   enableACME = true;
+      root = "/home/buhduh/app/current/public";
+      location."/" = {
+        recommendedProxySettings = true;
+        extraConfig  = ''
+            if (-f $request_filename) {
+              break;
+            }
+
+            if (-f $request_filename/index.html) {
+              rewrite (.*) $1/index.html break;
+            }
+
+            if (-f $request_filename.html) {
+              rewrite (.*) $1.html break;
+            }
+
+            if (!-f $request_filename) {
+              proxy_pass http://buhduh;
+              break;
+            }
+         '';
+      };
+      extraConfig = ''
+        index index.html;
+        try_files $uri $uri/ =404;
+      '';
+    };
   };
-
-  services.nginx.upstreams.buhduh.servers = { "unix:///tmp/puma.sock" = { };};
-
 }
