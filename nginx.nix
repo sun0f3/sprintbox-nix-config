bed74658922f64e9cced50b83bb4d18ea11a937e
Your Name <sun@Evgenijs-MacBook-Pro.local>
2023-04-07 09:39:46 +0300

    rename to host

--- nginx.nix
+++ nginx.nix
@@ -14,7 +14,8 @@
         #recommendedProxySettings = true;
         extraConfig  = ''
           proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-          proxy_set_header Host $http_host;
+          proxy_set_header Host $host;
+
           if (-f $request_filename) {
             break;
           }
