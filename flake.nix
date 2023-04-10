d21a0337a8ff94f34a0a39af78f2d6c75a357a40
Your Name <sun@Evgenijs-MacBook-Pro.local>
2023-04-10 18:32:21 +0300

    add brackets

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
         ({ pkgs, ... } : {
           environment.systemPackages = with pkgs; [
             gnumake
-            ruby_3_2.withPackages (ps: with ps; [ bundler ])
+            (ruby_3_2.withPackages (ps: with ps; [ bundler ]))
           ];
         })
         home-manager.nixosModules.home-manager
