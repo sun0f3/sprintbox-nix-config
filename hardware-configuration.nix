d489831712959dc70ca9e1af08725625aca4d8cb
Your Name <sun@Evgenijs-MacBook-Pro.local>
2023-04-10 09:54:50 +0300

    add swap

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -3,4 +3,9 @@
   imports = [ (nixpkgs + "/nixos/modules/profiles/qemu-guest.nix") ];
   boot.loader.grub.device = "/dev/vda";
   fileSystems."/" = { device = "/dev/vda3"; fsType = "ext4"; };
+
+  swapDevices = [{
+    device = "/var/lib/swapfile";
+    size = "1024";
+  }];
 }
