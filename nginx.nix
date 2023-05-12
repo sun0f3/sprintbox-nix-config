ab59204166875df23430432d79bb0dd96c51ebd9
Your Name <sun@Evgenijs-MacBook-Pro.local>
2023-05-12 17:47:40 +0300

    fix nginx

--- nginx.nix
+++ nginx.nix
@@ -15,7 +15,7 @@
         proxy_pass http://buhduh;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_set_header Host $host;
-        X-Forwarded-Proto http;
+        proxy_set_header X-Forwarded-Proto http;
         proxy_redirect off;
       '';
       extraConfig = ''
